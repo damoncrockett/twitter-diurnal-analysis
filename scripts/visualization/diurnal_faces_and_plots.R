4159ca6b35038d665243917f75867a6f47c49dc0
Mehrdad Yazdani <myazdani@gmail.com>
2015-07-07 19:40:39 -0700

    added correct theme

--- scripts/visualization/diurnal_faces_and_plots.R
+++ scripts/visualization/diurnal_faces_and_plots.R
@@ -4,19 +4,33 @@
 library(data.table)
 library(plyr)
 library(ggplot2)
-#dt = fread("~/Documents/twitter_data_grant/processedData/top60_faces_alt_tree_HSV.csv", header = TRUE)
-dt = fread("./data/Top_60_faces_alt_and_alt_tree_HSV_modes.csv", header = TRUE)
+dt = fread("~/Documents/twitter_data_grant/processedData/Top_60_faces_alt_and_alt_tree_HSV_modes.csv", header = TRUE)
 dt.clean = na.omit(dt)
 
 normalized.num.faces = function(df){
-  return(data.frame(hour = df$hour[1], city = df$city[1],num.rows = nrow(df), num.faces = sum(df$num_faces), 
-                    face.present = length(which(df$num_faces > 0)), solo.face = length(which(df$num_faces == 1)),
-                    social.face = length(which(df$num_faces > 1))))
+  return(data.frame(hour = df$hour[1], city = df$city[1],num.rows = nrow(df), num.faces = sum(df$num_faces_alt), 
+                    face.present = length(which(df$num_faces_alt > 0)), solo.face = length(which(df$num_faces_alt == 1)),
+                    social.face = length(which(df$num_faces_alt > 1))))
 }
 
 faces.hours = ddply(dt.clean, .(hour, city), normalized.num.faces)
 
-p = ggplot(faces.hours, aes(x = as.factor(hour), y = num.faces/num.rows)) + geom_point() + facet_wrap(~city, scales = "free") + ylab("Number of faces normalized by number of images") + xlab("hour") + theme(axis.text.x = element_text(size=6))
+p = ggplot(faces.hours, aes(x = as.factor(hour), y = num.faces/num.rows)) + 
+  geom_point(size = .8) + facet_wrap(~city, scales = "free", nrow = 6) + 
+  theme(strip.text.x = element_text(size = 6),
+        strip.background = element_blank(),
+        axis.line=element_blank(),
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.position="none",
+        panel.background=element_blank(),
+        panel.border=element_blank(),
+        panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        plot.background=element_blank())
 
 p = ggplot(faces.hours, aes(x = as.factor(hour), y = solo.face/num.rows)) + geom_point() + facet_wrap(~city, scales = "free") + ylab("Number of solo faces normalized by number of images")
 
